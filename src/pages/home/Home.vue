4c5a55576196ee2b04c696209dd403665c42cd39
yanzi <1603681786@qq.com>
2022-04-30 16:50:58 +0800

    favicon and navvar

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,12 +1,26 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <h2>首页</h2>
+  </div>
 </template>
 
 <script>
+  import NavBar from 'components/common/navbar/NavBar'
+
   export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+      NavBar
+    }
   }
 </script>
 
 <style scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
